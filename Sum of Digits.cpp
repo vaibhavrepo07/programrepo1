537d91d52e677b13c2bbdffe9403a3529461e4a2
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-01-19 14:49:37 +0530

    Sd

---
+++ Sum of Digits.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,  b  , c ;
+	cout<<"Addition of Three Numbers is ="<<endl;
+	cin>>a>>b>>c;
+	int ans;
+	ans=a+b+c;
+	
+	cout<<"Addition is ="<<ans<<endl;
+	
+	return 0;
+}

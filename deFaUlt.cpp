fe1bb16021ad20f06ffe23a82469603926fd33b5
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-01-20 17:00:51 +0530

    default

---
+++ deFaUlt.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int add(int x=100, int y=200, int z=300)
+{
+	return (x+y+z);
+}
+int main()
+{
+	cout<<add();
+	cout<<endl;
+	cout<<add(700);
+	
+	cout<<endl;
+	cout<<add(1110);
+	
+	
+	
+	return 0;
+}

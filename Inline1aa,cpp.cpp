071c4dbdae39c8eb12c3eabac5c5e611a7526c37
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-01-20 17:13:20 +0530

    Inl

---
+++ Inline1aa,cpp.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+inline int max(int x, int y)
+{
+	return ((x>y)? x : y);
+}
+
+   int main()
+  {
+  	 int a, b;
+  	 cout<<"Enter two Values";
+  	 cin>>a>>b;
+  	 cout<<"Greater value is "<<max(a,b)<<endl;
+  	 
+  	 return 0;
+  }

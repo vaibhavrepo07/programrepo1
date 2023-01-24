645e595a8a936d573a54365ca433018a7d5feb86
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-01-24 22:01:38 +0530

    for3a

---
+++ for3.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"fdd""dfs""fgasrf"<<endl;
+	cin>>n;
+	
+	for(int a=0,b=1,c=10 ; a<=3&&b>=5&&c>=4 ; a--,b--,++c)
+	{
+		cout<<c<<endl;
+	}
+	return 0;
+}

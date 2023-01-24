26fb1d38a5533ecc9fe5f1eef2e15b8b556db0a6
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-01-24 21:59:25 +0530

    continue1

---
+++ continue1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	for(int i=0; i<=3;i++)
+	{
+		cout<<"1"<<endl;
+		cout<<"2"<<endl;
+		cout<<"3"<<endl;
+		continue;
+		cout<<"4"<<endl;
+	}
+	
+	return 0;
+}

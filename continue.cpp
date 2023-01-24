b333fb34a649c40994263b3d70eeccce04dd69de
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-01-24 21:58:35 +0530

    continue

---
+++ continue.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+	for(int i=0;i<=4;i++)
+	{
+		cout<<"Hi"<<endl;
+		cout<<"Hey"<<endl;
+		cout<<"Hello"<<endl;
+		continue;
+		cout<<"Reply"<<endl;
+	}
+	
+	return 0;
+}

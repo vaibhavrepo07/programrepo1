29a020c082d27c888cf987eaa5335298b5b41ee2
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-01-31 22:13:17 +0530

    switchchn

---
+++ Switchchn.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n = 1;
+	char ch = '3';
+	
+	switch (ch)
+	{
+		case 1 : cout<<"First";
+		break;
+		
+		case '3': cout<<"Second";
+		break;
+		
+		default : cout<<"End ";
+		break;
+	}
+	return 0;
+}
---
+++ Switchforch.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char ch='2';
+	
+	cout<<endl;
+    switch(ch)
+                 {
+                 	case 1: cout<<"First";
+                 	break;
+                 	
+                 	case '2':cout<<"Second";
+                 	break;
+                 	
+                 	case 3 :cout<<"Third";
+                 	break;
+                 	
+                 	default:
+                 		cout<<"End of Switch";
+				 }
+				 
+				 return 0;
+}

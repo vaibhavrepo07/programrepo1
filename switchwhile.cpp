d4690aa517f0a064731cdf171a922b9ae8f1f69c
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-01-31 22:06:48 +0530

    PatternWhile

---
+++ switchwhile.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int ab = 1;
+	
+	switch(ab)
+	{
+		case 1: cout<<"First ";
+		break;
+		
+		case 2: cout<<"Second";
+		continue;
+		
+		case 3: cout<<"Third";
+		continue;
+		
+		default : cout<<"Hello";
+		continue;
+	}
+	return 0;
+}

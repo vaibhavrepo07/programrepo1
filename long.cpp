ff92199a95509d1ff4712884f2deaa31e3a1df4b
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-01-31 22:22:47 +0530

    long1

---
+++ long.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char ch = '1';
+	cout<<endl;
+	switch(ch)
+	{
+		case 1:cout<<"First"<<endl;
+		break;
+		
+		case '1':cout<<"Second"<<endl;
+		break;
+		
+		default: cout<<"End of the statement"<<endl;
+		
+	}
+	return 0;
+}

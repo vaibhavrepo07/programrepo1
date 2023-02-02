4c21898838d262407e5a2871f627ee2ce62b2c68
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-02 21:54:43 +0530

    Array4

---
+++ Array4.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getMin(int num[], int n)
+{
+	int max=INT_MAX;
+	
+	for(int i=0;i<=n;i++)
+	{
+		if(num[i]>max)
+		{
+			max=num[i];
+		}
+	}
+	
+	return max;
+}
+ int main()
+ {
+ 	int size;
+ 	cin>>size;
+ 	
+ 	int num[100];
+ 	
+ 	//taking input in array
+ 	for(int i=0;i<size;i++)
+ 	{
+ 		cin>>num[i];
+	 }
+	 cout<<"Maximum value is ="<<getMax(num,size)<<endl;
+	 cout<<"Minimum value is ="<<getMin(num,size)<<endl;
+	 
+	 return 0;
+ }
+

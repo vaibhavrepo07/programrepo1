b96c19bf005c590d0c03215aac8bee69e2ed93e9
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-02 21:56:45 +0530

    breaka1

---
+++ breaka1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	for(int i=0;i<=5;i++)
+	{
+		cout<<"Hello"<<endl;
+		cout<<"Hey "<<endl;
+		cout<<" HI"<<endl;
+       break;
+		cout<<"Alas!"<<endl;
+	}
+	return 0;
+}

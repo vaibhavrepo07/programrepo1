7a33d777a378c8295d1d695d2f72d5717c3addd9
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-02 22:00:07 +0530

    stl1

---
+++ stl1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+namespace vaibhav {
+	int val = 50;
+}
+   int main()
+   {
+   	  double val = 10.9;
+   	  cout<<val<<endl;
+   	  
+   	  return 0;
+   }

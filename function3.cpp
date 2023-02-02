e63554825c1e73615dab5d328257886be66ab78f
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-02 22:01:33 +0530

    Function3

---
+++ function3.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void add(int a, int b)
+{
+	int c;
+	c=a+b;
+	cout<<"Addition = "<<c;
+}
+
+  void add(int , int );
+  int main()
+  {
+  	int a,b;
+  	cin>>a>>b;
+  	add(a,b);
+  	
+  	return 0;
+  }

632963b2c7b02ee8cf5b76ed1d8935c7424bf4b7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-02 21:57:55 +0530

    fib1

---
+++ fib1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n=10;
+	
+	int a=0;
+	int b=1;
+	
+	cout<<a<<" "<<b<<" "<<endl;
+	
+	for(int i=1; i<=n ;i++)
+	{
+		int nextNumber=a+b;
+		cout<<nextNumber<<"  "<<endl;
+		
+		a=b;
+		b=nextNumber;
+	}
+	return 0;
+}

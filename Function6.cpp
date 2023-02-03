71d5566aab3a021163cc728909d15db40af04cd7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-03 22:38:04 +0530

    Function6

---
+++ Function6.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void area(int x, int y)
+{
+	int c;
+	c=x+y;
+	cout<<c<<""""""""""""<<endl;
+}
+
+  
+   int main()
+   {
+   	 
+   	  int a,b;
+   	  cin>>a>>b;
+   	  
+   	  area(a,b);
+   	  
+   	  cout<<area;
+   }

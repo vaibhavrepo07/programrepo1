f8e25fa84a6cc90d0577c2ebb96eb6f7e4d80052
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-03 22:38:41 +0530

    Function7

---
+++ Function7.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(void)
+{
+	
+	int a,b,c;
+	cin>>a>>b;
+	c=a+b;
+	
+	return c;
+}
+ 
+  int main()
+  {
+  	int ans;
+  	ans=add();
+  	
+  	cout<<ans;
+  	
+  	return 0;
+  }

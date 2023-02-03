7c84dc331c2455b70116d14ce1e3b1461dda1211
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-03 22:34:27 +0530

    Function2

---
+++ function2.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(int a, int b)
+{
+    int res;
+  	
+	res=a+b;
+	
+}
+  
+   int main()
+   {
+   	  int a,b;
+   	  cin>>a>>b;
+   	  
+   	  int answer = add(res);
+   	  cout<<"Answer is ="<<answer<<endl;
+   	  
+   	  return 0;
+   	  
+   }

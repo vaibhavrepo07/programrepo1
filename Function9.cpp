3b2f62656419a06adb0267bbe7f08f7daffcd8de
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-04 23:01:34 +0530

    Function9

---
+++ Function9.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(void)
+{
+	int a,b,c; 
+	cin>>a>>b;
+	c=a+b;
+	return (c); 
+}
+
+  int main()
+  {
+  	int ans ;
+  	
+  	ans=add();
+  	
+  	cout<<ans<<"   "<<endl;
+  	
+  	return 0;
+  }

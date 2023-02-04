c65b5a0005ecac147ad7e44c3fbf3e1a25a38a97
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-04 22:55:30 +0530

    UpdateArray

---
+++ Updatearray.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void update(int arr[], int n)
+{
+	cout<<"Inside the Function"<<endl;
+    //printing the array
+    
+    arr[0]=120;
+
+   //printing the array
+   
+    for(int i=0;i<3;i++)
+    {
+    	cout<<arr[i]<<" ";
+	}cout<<endl;
+	
+	cout<<"Going back to main Function";
+}
+
+int main()
+{
+	int arr[3]={1,2,3};
+	
+	update(arr,3);
+	
+	//printing the array
+	for(int i=0;i<3;i++)
+	{
+		cout<<arr[i]<<"  ";
+	}
+	cout<<endl;
+	
+	return 0;
+}

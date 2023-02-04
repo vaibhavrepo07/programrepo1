1961ce63647bca701f49e465a286b881af830451
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-04 22:56:14 +0530

    SizeofArrayWithSizeofOperator

---
+++ SizeofArraywithSizeofOperator.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printArr(int arr[] , int size)
+{
+	for(int i=0;i<=n;i++)
+	{
+		cout<<arr[i]<<"   ";
+	}
+}
+
+
+  int main()
+  {
+  	 int fifth [10]={1};
+  	 n=10;
+  	 
+  	 printArray(fifth, 10);
+  	 
+  	 int fifthSize = sizeof(fifth)/sizeof(int);
+  	 cout<<"Size of the Fifth is ="<<fifthsize<<endl;
+  	 
+  	 return 0;
+  }

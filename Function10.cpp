169183e9be14cb74c7e26af3814c60f75ac3b097
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-04 23:02:37 +0530

    Function10

---
+++ Function10.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void cube(int x)
+{
+	
+	
+	cout<<x*x*x<<""""""""<<endl;
+}
+
+  int main()
+  {
+  	  int c;
+  	  cin>>c;
+  	  
+  	  cube(c);
+  	  
+  	  return 0;
+  }

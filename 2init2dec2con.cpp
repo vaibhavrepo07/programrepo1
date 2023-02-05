402e3bfab68e9553dcbc0f16770b88224b52dfff
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-05 22:17:15 +0530

    2inti2dec2con

---
+++ 2init2dec2con.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<" ::: "<<endl;
+	cin>>n;
+	
+	for(int a=0,b=1; a<=0 && b>=1 ; a--,b--)
+	{
+		cout<<endl<<b<<endl;
+		
+	}
+	
+	return 0;
+}

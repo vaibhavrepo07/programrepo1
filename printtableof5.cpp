7fad45eb22b9434d535a22cba246852908f54411
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-05 22:18:21 +0530

    printtableof5

---
+++ printtableof5.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i , j ;
+	cout<<"Enter the Number "<<endl;
+	cin>>i;
+	j=1;
+	while(j<=10)
+	{
+		cout<<"  "<<i*j<<endl;
+		j=j+1;
+	}
+	return 0;
+}

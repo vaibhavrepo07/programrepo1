ad0e5b5f4d3b3b68d025438e60cdfcf8da356493
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-05 22:12:01 +0530

    FunctionWRNA1

---
+++ FunctionWRNA1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(void)
+{
+	int a, b ,c;
+	cin>>a>>b;
+	c=a+b;
+	
+	return c;
+}
+
+  int main()
+  {
+  	  int ans ;
+  	  
+  	  ans=add();
+  	  
+  	  cout<<ans;
+  }

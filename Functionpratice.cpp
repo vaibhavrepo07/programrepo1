2db2089e5b9f5bf8af11688a10f09480807a77e1
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-05 22:10:13 +0530

    Functionp

---
+++ Functionpratice.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sqr(int x)
+{
+	return (x*x);
+}
+
+int main()
+ 
+ {
+	int no, ans;
+	cout<<"Enter a Number";
+	cin>>no;
+	ans=sqr(no);
+	cout<<"Square of"<<no<<"is"<<ans;
+	
+	return 0;
+}
+
+
+
+

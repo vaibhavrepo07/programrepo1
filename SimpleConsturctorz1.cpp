1cb6991aa6145e627c411487659359ce3bd70eb6
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-06 20:27:02 +0530

    Constructors1

---
+++ SimpleConsturctorz1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class laptop
+{
+	public:
+		laptop ()
+		{
+			cout<<"LAPTOP ="<<endl;
+			cout<<"It stands for Light Weight Analytical Platform of Total Omtimized Power";
+		}
+		
+};
+  
+    int main()
+    {
+    	 laptop l1;
+    	 
+    	 return 0;
+	}

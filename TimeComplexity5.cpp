accef32c8b3f71189229fe5099b2e8aa5c2de017
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-06 20:23:53 +0530

    Timecomplexity

---
+++ TimeComplexity5.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printCounting(int n)  //Time Complexity = O(n).
+
+{    
+	int num;
+	for(int i=1; i<=num; i++)
+	{
+		cout<<i<<"  ";
+	}
+	cout<<endl;
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	
+	//function call 
+	printCounting(n);
+	
+	return 0;
+}

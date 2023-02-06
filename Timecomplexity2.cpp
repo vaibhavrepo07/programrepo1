e84983aa4b05570078c97c5ad7c042d85a6cf280
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-06 20:20:51 +0530

    TimeComplexity2

---
+++ Timecomplexity2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()         //Time Complexity = O(m+n).
+{
+	int N,M; int a=1,b=2;
+	
+	for(int i=0;i<N;i++)
+	{
+		a=a+rand();
+	}
+	for(int j=0;j<M ;j++)
+	{
+		b=b+rand();
+	}
+	
+	return 0;
+}

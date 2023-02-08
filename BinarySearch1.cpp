db756f0402acdb96677b65ce055fef299f9fce30
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-08 22:57:45 +0530

    BinarySearch1

---
+++ BinarySearch1.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key){
+	int start = 0;
+	int end = size-1;
+	
+	int mid = (start +end)/2;
+	
+	while(start<=end){
+		if(arr[mid]==key){
+			return mid;
+		}
+		//go to right wala part 
+		if(key = arr[mid]){
+			start=mid+1;
+		}
+		else{
+			end= mid-1;
+		}
+		mid=(start + end)/2;
+	}
+	return -1;
+}
+  int main()
+  {
+  	 int even[6]={2,4,6,8,12,18};
+  	 int odd [5]={3,8,11,14,16};
+  	 
+  	 int evenIndex = binarySearch(even,6,20)
+  	 
+  	 cout<<"Index of 18 is ="<<evenIndex<<endl;
+  	 
+  	 int oddIndex = binarySearch(odd , 5, 20);
+  	 
+  	 cout<<"Index of 20 is ="<<oddIndex<<endl;
+  	 
+  	 return 0;
+  }

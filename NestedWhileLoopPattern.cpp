6785d2b2b0ab6578fd989ff6f36df658a25ac373
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-08 23:02:41 +0530

    NestedWhileLoop

---
+++ NestedWhileLoopPattern.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i,j;
+	cin>>i;
+	i=1;
+	while(i<=10)
+	{
+		cout<<" *";
+		i++;
+	}
+	j=i;
+	while(j<=10)
+	{
+		cout<<" &";
+		j++;
+	}
+	return 0;
+}

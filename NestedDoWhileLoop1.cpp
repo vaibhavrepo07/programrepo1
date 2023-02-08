8d05a82f82015eb67ada3c761644f203cd1702bd
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-08 23:00:30 +0530

    NestedDoWhileLoop1

---
+++ NestedDoWhileLoop1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int i,j=1;
+   do
+   {
+   	i=1;
+   	do
+   	{
+   		cout<<"*";
+   		i++;
+   		}while(i<=j);
+	   
+	   cout<<"\n";
+	   j++;
+	   }while(j<=5);
+	   
+	   return 0;
+   }

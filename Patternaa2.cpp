9557ae90d8786e01fe0d2aedbedd19b399008067
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-09 22:55:03 +0530

    patternaa2

---
+++ Patternaa2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	for(int i =1; i<=5; i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			if(i==3 && j==3)
+			cout<<"o";
+			else
+			cout<<"x";
+		}
+		cout<<endl;
+	}
+	return 0;
+}

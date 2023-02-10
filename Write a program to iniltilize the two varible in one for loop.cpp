0726b06f9a8f7b5ff10f5709de4120c035858fef
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-10 22:18:57 +0530

    InitilizationTwovariableinoneLoop

---
+++ Write a program to iniltilize the two varible in one for loop.cpp
@@ -0,0 +1,13 @@
+//Write a program to Initilize the two varible in one for loop
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"The Two varible we have to initilize in For loop"<<endl;
+	for(int i=1, j=0; i<=5; i++)
+	{
+		cout<< i<< j<< endl;
+		
+	}
+	return 0 ;
+}

4ae36a0ce6c7a64d49e8d5c726080c4d396b846f
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-10 22:26:49 +0530

    UnderstandinForLoop

---
+++ Write a program to make us understand in for loop  .cpp
@@ -0,0 +1,14 @@
+//Write a program of For Loop to make us understand 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i , j=0 ;
+	cin>> i ;
+	for( ; i<=5 ;i++)
+	{
+		cout<< i << j   << endl;
+		
+	}
+	return 0 ;
+}

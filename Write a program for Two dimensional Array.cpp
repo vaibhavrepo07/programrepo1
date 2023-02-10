afb6ba35808c9ae419d98f4fe257a42cfff0093e
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-10 22:17:14 +0530

    Two DimensionalArray

---
+++ Write a program for Two dimensional Array.cpp
@@ -0,0 +1,15 @@
+//Program for Two Dimensional Arrays 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+      int a[] [3]={ 1,1,1,2} ,i;
+	  cout<<"Coding with Tow Dimensional Array"<<endl;
+	   for(i=0 ; i<3;i++)
+	   {
+	   	cin>>a[i];
+		   }	
+		   cout<<a[i]<<endl;
+		   
+		   return 0 ;
+} 

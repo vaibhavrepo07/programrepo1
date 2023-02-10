cbb3ae93d25aeea6f48ec5f264670079f27608cb
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-10 22:15:06 +0530

    Program for Stirng

---
+++ Write a program for String .cpp
@@ -0,0 +1,13 @@
+//Program for String Constant in C++
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char name[20], fname[30];
+	cin>> name;
+	cout<<"Enter the name "<<endl;
+	
+	cout<<name<<fname<<endl;
+	
+		return 0 ;
+}

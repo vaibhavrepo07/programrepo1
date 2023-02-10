a4aaecca0e114546721a09c25d61de1a05efd239
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-10 22:20:24 +0530

    AddnNumbers

---
+++ Write a program to print Add n numbes.cpp
@@ -0,0 +1,15 @@
+//Write a program to print Add n Numbers
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i , n, sum=0;
+	cout<<"How many numbers ?"<<endl;
+	cin>>n;
+	for(i=1; i<=n;++i)
+	{
+		sum=+i;
+	}
+	cout<<"Sum="<<sum;
+	return 0;
+}

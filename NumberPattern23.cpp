3149aaac14db5b74ec37b2dc629e60df5a8f3898
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-15 21:48:52 +0530

    NumberPattern23

---
+++ NumberPattern23.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n=7, k=2;
+	
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{
+			cout<<k+1<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+
+
+

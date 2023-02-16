8adef2d7d784d257e909a9ec8ae566a699d7ff44
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-16 22:33:53 +0530

    ActualArgumentandFormalArgument

---
+++ ActualArgumentandFormalArgument.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void add(int ,int );
+int main()
+{
+	int a, b;
+	cout<<"Enter Number";
+	cin>>a>>b;
+	add(a,b);
+	
+	return 0;
+}
+  void add(int x ,int y)
+  {
+  	int c ;
+  	c = x + y;
+  	cout<< c <<endl;
+  }

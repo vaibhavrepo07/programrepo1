80c729d2bab0034de10e07aae6419038c64a50b8
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-16 22:36:55 +0530

    AlphabetAtoE

---
+++ AlphabetuptoAtoE.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char i,j;
+	
+	for(int i ='A'; i<='E'; i++)
+	{
+		for(int j='A';j<='E';j++)
+		{
+			cout<<char(i);
+		}
+		cout<<endl;
+	}
+	return 0;
+}

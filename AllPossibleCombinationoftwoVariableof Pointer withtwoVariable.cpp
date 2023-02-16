d721db765afb153862064580f6eb2c071cbdf61d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-16 22:35:05 +0530

    All PossibleCombinationoftwoVAriable

---
+++ AllPossibleCombinationoftwoVariableof Pointer withtwoVariable.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int x=5, *j;
+	j=&x;
+	cout<<"The Value of x is "<<x<<endl;
+	cout<<"The Address of x is "<<&x<< endl;
+	
+	return 0;
+}

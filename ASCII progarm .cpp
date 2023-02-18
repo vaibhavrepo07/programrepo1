721b43f98d413d7206d3f2c279db0ea23f909efb
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-18 21:18:46 +0530

    ASCII program

---
+++ ASCII progarm .cpp
@@ -0,0 +1,13 @@
+//Print the ASCII Value of c++
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char c;
+	cout<<"Enter a character"<<endl;
+	cin>>c ;
+	cout<<"ASCII Value of "<<c<<"is"<<int (c)<<endl;
+	
+	return 0 ;
+	
+	}

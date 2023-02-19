cecff723a5e52486ec9e5ab893bba0f105979049
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-19 21:32:37 +0530

    CompA

---
+++ CompAsign.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+	int num=25;
+	
+	cout<<"Num :"<<num<<endl;
+	num+=10;
+	cout<<"Num +=10"<<num<<endl;
+	num-=5;
+	cout<<"Num -=10"<<num<<endl;
+	num*=4;
+	cout<<"Num *=10"<<num<<endl;
+	
+	return 0;
+	
+}

e46b52516cea29c84cc7943623faaddb4aff5116
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-19 21:31:01 +0530

    classscoperesolution

---
+++ ClassSCopeResolutionOperator.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+class test
+{
+	public:
+		void disp();
+};
+   
+    void test :: disp()
+    {
+    	cout<<"Hello World"<<endl;
+	}
+	
+	int main()
+	{
+		cout<<"HI";
+		return 0;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

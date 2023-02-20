66e2cfc466b2e057b90ce9e98046c05cf701b307
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-20 21:53:51 +0530

    Template

---
+++ Funciton template2 with float value.cpp
@@ -0,0 +1,13 @@
+//Program for Function template
+#include<bits/stdc++.h>
+using namespace std;
+void sum(int a  , int b)
+{
+	cout<<"The total is :"<< a+b <<endl;
+}
+int main()
+{
+	sum(2.3 , 4.3);
+	
+	return 0 ;
+}

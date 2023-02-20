ea488f6f7fe55ac4a055de2449330e5d7e5431f3
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-20 21:53:06 +0530

    lOop1to10

---
+++ For LOop for counting 1 to 10 .cpp
@@ -0,0 +1,20 @@
+//Progarm for For loops in c++ 
+//For Loop is Entry control loop
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int i=1;
+	cout<<"The Adroable Loop for Every Programmer is For Loop"<<endl;
+	 
+	for ( ;i<=10 ; )
+	{
+	
+		cout<<i<<endl;
+		i++;
+		
+	}
+	return 0;
+}

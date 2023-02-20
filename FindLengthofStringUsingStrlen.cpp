408cacfb14aeddef13374dbef4dfdccd3ba91076
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-20 21:50:06 +0530

    LengthofString

---
+++ FindLengthofStringUsingStrlen.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char str[40];
+	int length;
+	
+	cout<<"Enter a String";
+	cin.getline(str,40);
+	length = strlen(str);
+	
+	cout<<"Length of the String is "<<length<<endl;
+	
+	
+	 return 0;
+}
+

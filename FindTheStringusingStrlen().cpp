3cd4f47bd1c64f523958187939b77209aa9d7fbf
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-02-20 21:51:18 +0530

    Stringlen()

---
+++ FindTheStringusingStrlen().cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+ using namespace std;
+  int main()
+  {
+  	  char s[20];
+  	  
+  	  cout<<"Enter the String"<<endl;
+  	  gets(s);
+  	  
+  	  cout<<"Name"<<endl;
+  	  
+  	  puts(s);
+  	  
+  	  return 0;
+  }

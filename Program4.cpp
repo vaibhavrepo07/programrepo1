daf7995ccef5652831e0d59ac2bff59161051d10
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-01 23:23:15 +0530

    Programp3

---
+++ Program4.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+    cout<<"The for Loop";
+  
+    for(int i=10;i<=n;i++)
+    {
+    	cout<<"The value of a is ="<<i;
+	}
+	return 0;
+}
+ 

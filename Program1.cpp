fde19c3603ffc5fa69bc798e5335854824c58f6c
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-01 23:21:44 +0530

    Programp1

---
+++ Program1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a, b ;
+	cin>>a>>b;
+	
+	cout<<"Enter the Two Numbers"<<endl;
+	
+	cout<<"The value of a is ="<<a<<endl;
+	cout<<"The value of b is ="<<b<<endl;
+	cout<<"The Summation of Two Numbers is="<<a+b;
+	
+	
+	return 0;
+}

224f8466e7cc29cdeae622edb6938e67d4dd3e70
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-01 23:26:16 +0530

    Programp6

---
+++ Program7.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int x=100, y=200;
+	cout<<setw(5)<<"x="<<setw(5)<<x<<endl;
+	cout<<"y ="<<y<<endl;
+	cout<<setw(10)<<"sum="<<setw(5)<<x+y;
+	
+	return 0;
+}

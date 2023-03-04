47dca08ba93015a2bbdff8813d4dd5b82182e604
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-04 23:24:45 +0530

    If2

---
+++ if2.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	
+	if(a>b)
+	{
+	cout<<"Enter the value of A "<<endl;
+		cout<<"A is Greater than B ";
+	}
+	if(a<b)
+	{
+	cout<<"Enter the value of B "<<endl;
+		cout<<"B is Greater than A";
+	}
+	return 0;
+}

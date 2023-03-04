3cc4a4bcb5c5f65feb0a407b07103f78793739bf
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-04 23:22:59 +0530

    If1

---
+++ if1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of a"<<endl;
+	cin>>a;
+	cout<<"Enter the value of b"<<endl;
+	cin>>b;
+	
+	if(a>b)
+	{
+		cout<<"A is greater than B"<<endl;
+	}
+	if(b>a)
+	{
+		cout<<"B is greater than A"<<endl;	
+	}
+	return 0;
+}
+

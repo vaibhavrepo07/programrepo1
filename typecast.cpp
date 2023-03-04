95ede233d837a77f612e98566fe73df8f5803c67
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-04 23:26:01 +0530

    typecast1

---
+++ typecast.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*type casting 
+int a ='a';
+cout<<a<<endl;
+
+   
+int main()
+{
+
+
+int a= 'a';
+cout<<a<<endl;
+
+char ch=98;
+cout<<ch<<endl;
+
+char ch1=123456;
+cout<<ch1<<endl;
+*/ 
+int main()
+{
+
+unsigned int a=112;
+
+cout<<a<<endl;
+
+return 0;
+}

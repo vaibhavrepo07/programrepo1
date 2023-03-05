3119b30feab7bc19c2a5f527a690132a65b01816
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-05 23:21:06 +0530

    ifa6

---
+++ ifa6.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a=9;
+	if(a==9)
+	
+	{
+		cout<<"Nine"<<endl;
+		
+	}
+	if(a>0){
+		cout<<"Positive"<<endl;
+	}
+	else
+	{
+		cout<<"Negative"<<endl;
+	}
+	
+	return 0;
+}
+

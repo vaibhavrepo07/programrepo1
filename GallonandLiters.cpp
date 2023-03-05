b3fd505d2555c9c0ea7f0bfdadbe8a8835b06dc5
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-05 23:16:47 +0530

    GallonandLiters

---
+++ GallonandLiters.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	float liters, gallon ;
+	
+	cout<<"Enter the Number of Gallons :"<<endl;
+	cin>>gallon;
+	
+	liters= gallon * 3.78;  // convert to liters
+	
+	cout<<"Liters:"<<liters <<endl;
+	
+	return 0;
+}

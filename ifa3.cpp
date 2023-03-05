fbd494fbe461f901a8544d28c2b965075d0fabd6
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-05 23:19:29 +0530

    ifa3

---
+++ ifa3.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin>>a;
+	
+	if(a>0)
+	{
+		cout<<"A is Positive";
+	}
+	else
+	{
+		if(a<0)
+		{
+			cout<<"A is Negative";
+		}
+		else
+		{
+			cout<<"A is 0(Zero)";
+		}
+	}
+	return 0;
+}

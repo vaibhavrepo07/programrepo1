c44bb542070b0261921551cedebc44f31762eb88
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-05 23:20:13 +0530

    ifa5

---
+++ ifa5.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a ;
+	cout<<"Coding with If Else statement"<<endl;
+	cin>>a;
+	
+	if(a>0)
+	{
+		cout<<"The value of A is Positive:"<<endl;
+		
+	}
+	else if(a<0)
+	{
+		cout<<"The value of  A  is Negative"<<endl;
+	}
+	else
+	{
+		cout<<"The value of A is Zero(0)"<<endl;
+	}
+	return 0;
+}

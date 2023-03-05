09ac3d03b4ae265627ffd6b4094c2d7e18842f6b
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-05 23:18:54 +0530

    ifa2

---
+++ Ifa2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+
+{
+	int a,b ;
+	
+	cout<<"Enter the value of a "<<endl;
+	cin>>a;
+	cout<<"Enter the value of b"<<endl;
+	cin>>b;
+	
+	if(a>b)
+	{
+		cout<<"A is Greater"<<endl;
+	}
+	if(b>a)
+	{
+		cout<<"B is Greater"<<endl;
+	}
+	
+	return 0;
+}

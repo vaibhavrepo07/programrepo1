ea5b4384ce1cc0b3d97ffe346c50c96289b57e1e
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-06 23:23:50 +0530

    whilea3

---
+++ whilea3.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int i=1;
+	cout<<"Lets' do coding with while Loop"<<endl;
+	
+	while(i<=10)
+	{
+		cout<<i<<endl;
+		i++;
+	}
+	return 0;
+}

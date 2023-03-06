3d509fac6f3bc3c9ff93b5b17823663cc4616d10
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-06 23:24:31 +0530

    whilea2

---
+++ whilea2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	
+	int a=1;
+	
+	while(a<=n)
+	{
+		cout<<"The value of n is ="<<a<<endl;
+		a=a+1;
+	}
+	return 0;
+}

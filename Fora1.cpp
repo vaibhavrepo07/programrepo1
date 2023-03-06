0a9c1abf796d809bd44047ed9bc15732e74e58ac
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-03-06 23:22:37 +0530

    Fora1

---
+++ Fora1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	
+	cout<<"Coding with For Loop"<<endl;
+	for(int i=1;i<=n;i++)
+	{
+		cout<<i<<endl;
+	}
+	return 0;
+}

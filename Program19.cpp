eaff4cefe1ef1cb773a96c0e4665f3d50187feb7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-04-16 23:43:49 +0530

    Program19

---
+++ Program19.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i=1, sum=0,n;
+	cin>>n;
+	while(i<=n)
+	{
+		
+        sum=sum+i;
+		i=i+1;
+	}
+	cout<<"Value of sum is ="<<sum<<endl;
+	
+	return 0;
+}

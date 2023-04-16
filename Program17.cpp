23adf036d81365c1e08ef6d4b8ea70c5fbaa85b2
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-04-16 23:49:00 +0530

    Program17andProgram18

---
+++ Program17.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	unsigned int a=123;
+	
+	cout<<a<<endl;
+	
+	return 0;
+}
---
+++ Program18.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a=18, b=9;
+	
+	cout<<"Value of a is greater"<<(a>b)<<endl;
+	cout<<"The value is ="<<('c'<'b')<<endl;
+	
+	return 0;
+}

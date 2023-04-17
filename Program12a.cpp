850c80081ef0880fb9db8869bf34077847d32f9e
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-04-17 21:54:33 +0530

    Pr2

---
+++ Program12a.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int  a;
+	cout<<"Enter one Number"<<endl;
+	cin>>a;
+	
+	if(a%2==0)
+	{
+		cout<<"Even Number"<<endl;
+	}
+	else
+	{
+		cout<<"Odd Number"<<endl;
+	}
+	return 0;
+}

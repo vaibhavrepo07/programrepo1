a2d6eeb6b734bedbf4c8c9e9c26f7656d8c4fcbf
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-04-17 21:44:44 +0530

    Program15

---
+++ Program15.cpp
@@ -0,0 +1,30 @@
+//Data types and Variables.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a=123;
+	
+	cout<<a<<endl;
+	
+	char b='v';
+	cout<<b<<endl;
+	
+	bool bl=true;
+	cout<<bl<<endl;
+	
+	float f=1.3;
+	cout<<f<<endl;
+	
+	double d=233.33;
+	cout<<d<<endl;
+	
+	int size = sizeof(a);
+	cout<<"Size of a is :"<<size<<Endl;
+	
+	
+	return 0;
+	
+}

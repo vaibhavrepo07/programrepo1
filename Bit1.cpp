739a23148865d6063b12c007f81df93adbfdd289
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-04-30 22:45:47 +0530

    Bit1.cpp

---
+++ Bit1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a=4;
+	int b=5;
+	
+	cout<<"Coding with Bitwise Operator"<<endl;
+	
+	cout<<(a&b)<<"  "<<endl;
+	cout<<(a|b)<<"  "<<endl;
+	cout<<(a^b)<<"  "<<endl;
+	cout<<(~a )<<"  "<<endl;
+
+	
+	return 0;
+}

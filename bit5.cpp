4a02e40ec7069dfe64e5c41a25a2042bd820d6c0
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-04-30 22:52:30 +0530

    Bit5

---
+++ bit5.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a=1;
+	int b=2;
+	
+	if(a-->0 && ++b>2)
+	{
+		cout<<"Stage1 -Inside if";
+	}else{
+		cout<<"Stage2-Inside else";
+	}
+	cout<<a<<"  "<<b<<endl;
+	
+	return 0;
+}

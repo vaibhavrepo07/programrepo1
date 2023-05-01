025778f8c75d6e7abb58108bc484e4a296ecbf7d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-01 06:57:18 +0530

    Fun1d

---
+++ fun1d.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sum(int a, int b)   //function definition
+{
+	 sum=0;
+	sum=a+b;            
+	
+	cout<<sum<<endl;
+}
+
+ void sum(int ,int );
+ void main()
+ {
+ 	sum(3,2);
+ 	
+ 	
+ }

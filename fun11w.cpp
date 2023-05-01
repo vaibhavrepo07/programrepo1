5b0e06b380cb7b0ffb190d7cdadda1ceb8cc7763
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-01 07:00:35 +0530

    Fun11w

---
+++ fun11w.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(void);
+
+void main()
+{
+	int s;
+	s=a+b;
+	cout<<a<<endl;
+}
+
+int sum(void )
+{
+	int a=4,b=5,sum=0;
+	sum=a+b;
+	return sum;
+}

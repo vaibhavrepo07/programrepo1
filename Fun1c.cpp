3206fc535445629d05aca15f43fd0540c9beea06
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-01 06:56:17 +0530

    Fun1c

---
+++ Fun1c.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void mul()
+
+{
+	int a, b;
+	int mul;
+	cin>>a>>b;
+	
+	mul=a*b;
+	
+	cout<<"The Product of two Numbers is =" <<mul<<endl;
+	
+}
+
+int main()
+{
+	 mul();
+	 
+	 return 0;
+}

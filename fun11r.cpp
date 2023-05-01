eab0d9725229eb84d476261a426d0142ad57e3f4
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-01 06:59:57 +0530

    Fun11r

---
+++ fun11r.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+void main()
+{
+	int x, y,c;
+	cout<<"Enter x and y"<<endl;
+	cin>>x>>y;
+	c=sum(x,y);
+	cout<< c<<endl;
+}
+
+int sum(int a , int b)
+{
+	return a+b;
+}

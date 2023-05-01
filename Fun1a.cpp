3fd60ea3aa4f19ad8bf245792792154c1523159d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-01 06:54:39 +0530

    Fun1a

---
+++ Fun1a.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sum()
+{
+	int a, b;
+	int sum=0;
+	cout<<"Enter two Numbers"<<endl;
+	cin>>a>>b;
+	sum=a+b;
+	cout<<"Summation of two numbers is ="<<sum<<endl;
+}
+  int main()
+  {
+  	sum();
+  	sum();
+  	
+  	return 0;
+  }

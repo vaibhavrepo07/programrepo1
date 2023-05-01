1c989c605786c3da437e20ee3dbd007148d64a41
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-01 06:52:29 +0530

    F1

---
+++ F1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	//power of a,b 
+	int a, b;
+	
+	cin>>a>>b;
+	
+	int ans =1;
+	for(int i=1 ;i<=b ;i++)
+	{
+	     ans = ans * a;
+	     
+	}    
+	cout<<"Answer is "<<ans <<endl;
+	
+	return 0;                                                         
+}
+

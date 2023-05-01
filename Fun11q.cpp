016432d88a1ae54658dcbf958970a474503fdce6
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-01 06:59:13 +0530

    Fun11q

---
+++ Fun11q.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sum();
+
+main()
+{
+	sum();
+	
+	return 0;
+}
+
+ void sum()
+ {
+ 	int a,b,sum=0;
+ 	cin>>a>>b;
+ 	sum=a+b;
+ 	cout<<"The sum of two numbers are"<<sum<<endl;
+ }

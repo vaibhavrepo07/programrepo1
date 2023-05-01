776c6513518e1b270cf731e2de81e36878d1e243
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-01 06:55:24 +0530

    Fun1b

---
+++ fun1b.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sub()
+{
+	int sub=0;
+	int a,b;
+	cout<<"Enter the subtraction"<<endl;
+	cin>>a>>b;
+	sub=a-b;
+	cout<<"The Difference is = "<<sub<<endl;
+	
+}
+    int main()
+    {
+    	sub();
+    	
+    	return 0;
+	}

82b29e2b86be9181c3ddedbc6d8d340e64aa12c0
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-03 08:39:13 +0530

    Swit8

---
+++ swit8.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a, b;
+	
+	cout<<"Enter the value of a "<<endl;
+	cin>>a;
+	
+	cout<<"Enter the value of b"<<endl;
+	cin>>b;
+	
+	char op;
+	cout<<"Enter the Operation you want to perform"<<endl;
+	cin>>op;
+	
+	switch(op)
+	{
+		case '+':cout<<(a+b)<<endl;
+		break;
+		
+		case '-':cout<<(a-b)<<endl;
+		break;
+		
+		case '*':cout<<(a*b)<<endl;
+		break;
+		
+		case '/':cout<<(a/b)<<endl;
+		break;
+		
+		default : cout<<"Here's the end"<<endl;
+		
+	}
+	cout<<endl;
+	
+	return 0;
+}

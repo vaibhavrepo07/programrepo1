8e11959000ab05438723bfaa20b17a4b3c849cbc
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-03 08:40:16 +0530

    Swit6

---
+++ swit6.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char ch = 'a';
+	cout<<endl;
+	
+	switch(ch)
+	{
+		case 'a': cout<<"Dell"<<endl;
+		break;
+		
+		case 'b': cout<<"HP"<<endl;
+		break;
+		
+		case 'c': cout<<"ASUS"<<endl;
+		break;
+		
+		case 'd': cout<<"Omen"<<endl;
+		break;
+		
+		default :cout<<"All are the standards laptops"<<endl;
+		break;
+	}
+	return  0;
+}

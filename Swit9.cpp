d15b62a7b6509ca9ac0347dca5d6f1a2b61f05e0
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-03 08:38:28 +0530

    Swit9

---
+++ Swit9.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	char ch = 5;
+	int num=1;
+	
+	switch(num)
+	{
+		case 1: cout<<"To begin with"<<endl;
+		//break;
+		
+		case '1': cout<<"In Addition "<<endl;
+		break;
+		
+		case 3: cout<<"Moreover"<<endl;
+		break;
+		
+		//default : cout<<"Certainly"<<endl;
+	}
+	
+	return 0;
+}

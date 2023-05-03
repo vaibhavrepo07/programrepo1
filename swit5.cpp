504728ed8e3a7d47de492c8d8ca25cb51a1512a9
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-03 08:42:11 +0530

    Swit5

---
+++ swit5.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char ch = 'a';
+	switch(ch)
+	{
+		case 'a' :cout<<"Blue"<<endl;
+		break;
+		
+		case 'b': cout<<"Red"<<endl;
+		break;
+		
+		case 'c': cout<<"Brown"<<endl;
+		break;
+		
+		default : cout<<"It is is Default value"<<endl;
+		
+	}
+	return 0;
+	
+}

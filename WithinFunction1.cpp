d7c9d0e8e956364620aab3cd66958db0256c5c12
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-03 08:35:58 +0530

    WithinFunction1

---
+++ WithinFunction1.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sub()
+{
+	int sub=0;
+	int a, b ;
+	cin>>a>>b;
+	sub=a-b;
+	cout<<sub<<endl;
+
+}
+  void add()
+  {
+  	int a,b;
+  	int add=0;
+	  cin>>a>>b;
+  	add=a+b;
+  	cout<<add<<endl;
+  }
+  
+  void main()
+  {
+  	 add ();
+  	 sub();
+  	 
+  	 return 0;
+  }
+  

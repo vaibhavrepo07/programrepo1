ead8f8bba9be9efc54e9e956f698e6a1d47450ba
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-04 11:52:01 +0530

    Swit1

---
+++ Swit1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int num=2;
+	
+	switch (num) {
+		
+		case 1:cout<<"First"<<endl;
+		break;
+		case 2:cout<<"Second"<<endl;
+		break;
+		
+		default : cout<<"It is default case"<<endl;
+		
+	}
+}

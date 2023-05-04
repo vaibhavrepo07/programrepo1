6be4f6a4d28e6e2aac6658a7fc3f54705a441d22
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-04 11:49:32 +0530

    Swit4

---
+++ swit4.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char ch= '1';
+	cout<<endl;
+	
+	switch(ch)
+	{
+		case 1:cout<<"First"<<endl;
+		break;
+		case 2: cout<<"Second"<<endl;
+		break;
+		case '1': cout<<"Third"<<endl;
+		break;
+		
+		default : cout<<"It is default case"<<endl;
+	}
+	cout<<endl;
+	
+	return 0;
+}
+

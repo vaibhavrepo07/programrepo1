b27141d57cb1496c53d92b11bb41fb322b3e3efc
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-04 11:50:23 +0530

    Swit3

---
+++ swit3.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int num=3;
+	cout<<endl;
+	
+	switch(num)
+	{
+	    case 1 :cout<<"Brother"<<endl;
+	    break;
+	    case 2: cout<<"How it's"<<endl;
+	    break;
+	    case 3: cout<<"Being Going"<<endl;
+	    break;
+	    
+	    default: cout<<"It is default case"<<endl;
+	    
+	}
+	return 0;
+}

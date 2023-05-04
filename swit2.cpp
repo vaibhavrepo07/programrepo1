1a011d6c0a76e2b8f9bb8b9c9fd9b10ade51842e
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-04 11:51:10 +0530

    Swit2

---
+++ swit2.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int num = 3;
+	cout<<endl;
+	
+	switch(num)
+	{
+		case 1: cout<<"Hello"<<endl;
+		break;
+		case 2: cout<<"Brother"<<endl;
+		break;
+		case 3: cout<<"How are you"<<endl;
+		break;
+		case 4: cout<<"Good"<<endl;
+		break;
+		
+		default :cout<<"It is default case"<<endl;
+	}
+	
+	return 0;
+}

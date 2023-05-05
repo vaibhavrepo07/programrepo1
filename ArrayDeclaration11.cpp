08039b07ae3eb938ff97d8a807f1823f8d487130
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-05 20:14:37 +0530

    Arrayd1

---
+++ ArrayDeclaration11.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printArray()
+{
+	
+}
+   int main()
+   {
+   	  int number [15];
+   	  
+   	  //accessing an Array
+   	  cout<<"Value at 14 index"<<number[14]<<endl;
+   	  
+   	  cout<<"Value at 30 index"<<number[30]<<endl;
+   	cout<<endl;
+   	cout<<endl;
+   	//Initilization of an Array
+   	
+   	int num[3]={   };
+   	
+   	cout<<"The position of second number is ="<<num[3]<<endl;
+   	  
+   	  int third[2]={4,6};
+   	  cout<<"The Position of Third Number is ="<<third[1]<<endl;
+   	  
+   	  //Initilizing all the values of Arrays with Zeros.
+   	  
+   	   int fourth [10]= {0};
+   	   
+		  int n=10;
+   	  
+		 cout<<"Printing the array"<<endl;
+   	  
+		 for(int i=0; i<=n; i++)
+		 {
+			  cout<<fourth[i]<<"   "<<endl;
+		 }
+		 cout<<"Everyting is Fine"<<endl;
+   	  
+   	  
+   	    //Initilization of all the Array elements with 1
+   	      
+   	      int fifth[10]={1};
+   	      
+   	      n=10;
+   	      cout<<"Printing the Array"<<endl;
+   	      
+   	      //Print the Array
+   	      for(int i=1;i<=n;i++)
+   	      
+   	      { 
+   	        cout<<fifth[i]<<"    "<<endl;
+			 }
+   	    
+   	    
+   	    
+   	    
+   	    
+   	    
+   	    
+   	    
+   	    
+   	  return 0;
+   }

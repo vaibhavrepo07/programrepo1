8139fd93b8b930de22a26900477fc7e82c9a32c5
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-05 20:23:12 +0530

    Practice2

---
+++ Practice2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void fun(int , int );
+int main()
+{
+	int a=4,b=2;
+	fun(a,b);
+	cout<<"The value of a and b"<<a<<b<<endl;
+}
+
+ void fun(int x, int y)
+ {
+ 	x=7;
+ 	y=5;
+ 	cout<<"x and y "<<x<<y<<endl;
+ }

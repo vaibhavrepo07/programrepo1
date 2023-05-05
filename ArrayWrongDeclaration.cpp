58ee22be494a47ad41df40f3718f0bbdc0fca95f
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-05 20:15:28 +0530

    ArrayWrongDec1

---
+++ ArrayWrongDeclaration.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+  int main()
+  {
+  	 
+  	 //declare 
+  	 int number[15];
+  	 cout<<"Value at 1 index"<<number[21]<<endl;
+  	 
+  	 cout<<endl<<"Everything is Fine"<<endl;
+  	 
+  	 return 0;
+  }

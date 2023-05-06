a27eb8cd1bac7c7afdb65b0189870e8875697bfc
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-06 22:18:29 +0530

    InitArray

---
+++ InitArray.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int no[5]={10,20,30,40,50};
+	
+	for(int i=0 ; i<5; i++)
+	{
+		cout<< i+1<< " ::::: "<< no[i]<<endl;
+	}
+	
+	return 0;
+}

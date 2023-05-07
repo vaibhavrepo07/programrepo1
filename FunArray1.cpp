352fe6a8762209daff27748191942a7604cb45ce
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-05-07 23:07:09 +0530

    FunArray1

---
+++ FunArray1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//hold
+void printArray(){
+	
+}
+int main()
+{
+	//declare an Array
+	int number[15];
+    
+	cout<<"Value at 0 Index"<<number[20]<<endl;
+	
+	cout<<endl<<"Everything is Fine"<<endl<<endl;	
+
+return 0;	
+}

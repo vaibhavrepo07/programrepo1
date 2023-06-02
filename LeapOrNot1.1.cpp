d3759bf23d2fccb4db659917a37943504a67914b
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-06-02 22:43:28 +0530

    LeaporNot1.1

---
+++ LeapOrNot1.1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Check Leap Year or Not"<<endl;
+	cin>>a;
+	
+	if(a%4==0)
+	{
+		cout<<"It is Leap Year"<<endl;
+	}
+	else
+	{
+		cout<<"It is not a Leap Year"<<endl;
+	}
+	return 0;
+}

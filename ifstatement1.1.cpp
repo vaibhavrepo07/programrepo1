0f30dcb623776f1084cf5d13fe74be3712c5319f
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-06-02 22:42:44 +0530

    ifstatement1.1

---
+++ ifstatement1.1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout<<"Simple If Statemet"<<endl;
+	cin>>a;
+	
+	if(a>100)
+	{
+		cout<<"A is Greater than 100"<<endl;
+	}
+	return 0;
+}

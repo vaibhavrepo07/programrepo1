f4c0dbc8f5286c871c4a175243fa05b37173ddc3
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-06-02 22:41:21 +0530

    Ifelse1.1

---
+++ ifelse1.1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a, b ;
+	cout<<"Coding with If else statement"<<endl;
+	cout<<"Enter the value of A "<<endl;
+	cin>>a;
+	cout<<"Enter the value of B "<<endl;
+	cin>>b;
+	
+	if(a>b)
+	{
+		cout<<"A is Greater than B"<<endl;
+	}
+	else
+	{
+		cout<<"B is not Greater than A"<<endl;
+	}
+	
+	return 0;
+}

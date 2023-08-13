3f7692ea3b8a35489e2d82a678a3dabb8971f66f
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-08-13 23:25:33 +0530

    Incopdecop

---
+++ IncopdecOp.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	//Increment and Decrement Operator .
+    
+    int i=7;
+    
+    cout << (++i) <<endl;
+    cout << (i++) <<endl;
+    cout << (i--) <<endl;
+    cout << (--i) <<endl;
+    
+    return 0;
+}

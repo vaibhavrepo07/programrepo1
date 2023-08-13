f5619376b27e04effca906fd7a3a50fd68b50e0b
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-08-13 23:19:42 +0530

    RefrenceAfterandBefore

---
+++ RefrencevarafterandBefore.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void update(int n){
+	n++;
+}       
+
+int main()
+{
+	int n = 5;
+	cout<<"Before "<< n<<endl;
+	update(n);
+	cout<<"After "<< n<<endl;
+	
+	return 0;
+	
+	
+												 }                                             

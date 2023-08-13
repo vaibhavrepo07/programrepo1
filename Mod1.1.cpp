0dc2c9e3c6ba5c870bf774a22f9cc1b1dde001e9
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-08-13 23:26:48 +0530

    Mod1.1a

---
+++ Mod1.1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a=10, b=7;
+	
+	cout<<"a+b ="<<a+b<<endl;
+	cout<<"a-b ="<<a-b<<endl;
+	cout<<"a*b ="<<a*b<<endl;
+	
+	
+	
+	int c=-10 , d=7;
+	cout<<"c % d = "<<c%d<<endl;
+	
+	int z=10 , q=7;
+	cout<<"z % q = "<<z%q<<endl;
+	
+	//int and float 
+	
+	int g=10 ;
+	float h=7;
+	
+	cout<<"g + h = "<<g+h<<endl;    //It is valid sum is = 17. 
+	
+//	cout<<"g % h = "<<g%h<<endl;  It shows Error .
+	
+	
+	return 0;
+}

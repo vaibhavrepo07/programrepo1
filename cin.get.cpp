f0ca0f5f650f54d5636afe374d1a7f7328c250b9
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-08-13 23:15:46 +0530

    cin.get

---
+++ cin.get.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+/*{
+	int a;
+	cin>>a;
+	
+	//cout<<"Value of n is :<< n <<endl;
+	
+	if(a>0)
+	{
+		cout<<"A is Positive:"<<endl;
+	}
+	else{
+		cout<<"A is Negative:"<<endl;
+	}
+
+	{
+	
+	int a,b;
+	a = cin.get();
+    	
+	cout<<"Value of a is ="<<a<<endl;
+	cout<<"Enter the value of A"<<endl;
+	cin>>a;
+	cout<<"Enter the value of B"<<endl;
+	cin>>b;
+	
+	if(a>b){
+		cout<<"A is Greater "<<endl;
+	}else{
+		cout<<"B is Greater "<<endl;
+	}
+
+   int a;
+   cout<<"Enter the Value of A"<<endl;
+   cin>>a;
+   
+   if(a>0){
+   	cout<<"A is Positive"<<endl;
+   }
+   else{
+   	cout<<"A is 0"<<endl;
+   }
+*/
+{
+ int a;
+ cout<<"Enter the value of a"<<endl;
+ cin>>a;
+ 
+ if(a>0)
+ {
+ 	cout<<"A is positive"<<endl;
+ }
+ else if(a<0)
+ {
+ 	cout<<"A is Negative"<<endl;
+ }
+ else{
+ 	cout<<"A is 0"<<endl;
+ }
+
+	return 0;
+}

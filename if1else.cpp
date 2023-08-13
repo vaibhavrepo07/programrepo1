4607b2f3d35f23895ab0735d46f0f5908b78c062
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-08-13 23:17:54 +0530

    if111lelse

---
+++ if1else.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout<<"Coding with If Else Statment"<<endl;
+	
+	cin>>a>>b;
+	
+	if(a>b)
+	{
+		cout<<"A is Greater than B"<<endl;
+	}
+	else{
+		cout<<"B is Greater than A"<<endl;
+	}
+	
+	return 0;
+}

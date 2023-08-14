c660f675ab45423ac9963b8d78674f35d42826bb
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-08-14 23:40:09 +0530

    2-D Array

---
+++ 2-D Array1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int a[2][3] , i,j;
+   cout<<"Coding with 2-D Array"<<endl;
+   for(i=0;i<=2;i++)
+   {
+   	for(j=0;j<=3;j++)
+   	{
+   		 cout<<" "<<a[i][j];
+	   }
+	   cout<<endl;
+   }
+   return 0;
+}

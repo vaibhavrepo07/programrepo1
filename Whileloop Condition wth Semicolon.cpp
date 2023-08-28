2a9829823a763bfe04161e22ab6f469239286873
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-08-28 17:22:06 +0530

    WhileLoopConditionwithSemicolon

---
+++ Whileloop Condition wth Semicolon.cpp
@@ -0,0 +1,13 @@
+//Program to While loop with semicolon 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i = 0 ;
+	while(++i);
+	{
+		cout<<i<<endl;
+	} 
+	cout<<"End of Program"<<endl;
+	return 0 ;
+}

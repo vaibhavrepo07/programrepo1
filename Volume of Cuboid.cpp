981c5ee80ece28b1d7a53e99fc4825669b5417e7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-08-28 17:20:28 +0530

    Volume of Cuboid

---
+++ Volume of Cuboid.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int l,w,h;
+	int volume ;
+	
+	cout<<"Enter lenght ,width,height"<<endl;
+	cin>>l>>w>>h;
+	
+	volume = l*w*h;
+	
+	cout<<"volume of Cuboid "<<volume <<endl;
+	
+	return 0;
+}

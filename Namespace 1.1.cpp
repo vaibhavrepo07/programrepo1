79c15b2785c4b3814366f8a7f23850ce6e3bd369
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-08-28 17:18:04 +0530

    Namespace1

---
+++ Namespace 1.1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+namespace first 
+{
+	void disp()
+	{
+		cout<<"Displying the Disp()"<<endl;
+	}
+	
+	void curr()
+	{
+		cout<<"Current the Position"<<endl;
+	}
+}
+
+int main()
+{
+	first::disp();
+	first::curr();
+	
+	return 0;
+}

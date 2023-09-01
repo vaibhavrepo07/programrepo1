cc0f7380eaa85e3471a9153737c8588d59a4e436
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-09-01 19:32:37 +0530

    Dj

---
+++ djfsd.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout<<"Enter the number"<<endl;
+	cin>>n;
+	int a=0;
+	while(a<=n)
+	{
+		cout<<a<<endl;
+		a++;
+	}
+	
+	return 0;
+}

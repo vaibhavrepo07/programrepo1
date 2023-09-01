7dcd4f6876387d7d1c188a37e1e5923e202b878c
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-09-01 19:31:37 +0530

    A

---
+++ Add.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Function Prototype :Return Type,Function Name,ParameterList,Terminating Semicolon
+
+int add(void)
+{
+	int a,b,c;
+	cin>>a>>b;
+	cout<<"Enter the Numbers a and b"<<endl;
+	c=a+b;
+	cout<<"The Summation of Two Numbers are ="<<c<<endl; 
+}
+
+  int main()
+  {
+  	int ans ;
+  	ans = add();
+  	
+  	return 0;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

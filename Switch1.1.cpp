2dac8772c1f8fd07bc19ddddc7bb6e89d7c13c90
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-09-01 19:30:52 +0530

    Sw

---
+++ Switch1.1.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout<<"Coding with Switch Statment"<<endl;
+	cin>>a;
+	
+	switch(a)
+	{
+		case 1:cout<<"Monday"<<endl;
+		break;
+		
+		case 2:cout<<"Tuesday"<<endl;
+		break;
+		
+		case 3:cout<<"Wednesday"<<endl;
+		break;
+		
+		case 4:cout<<"Thrusday"<<endl;
+		break;
+		
+		case 5:cout<<"Friday"<<endl;
+		break;
+		
+		case 6:cout<<"Saturday"<<endl;
+		break;
+		
+		case 7:cout<<"Sunday"<<endl;
+		break;
+		
+		default:
+			cout<<"Nothing is there"<<endl;
+	}
+	
+	return 0;
+}

40e717d73e4e0f04f059abac991f13ba1640641e
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-09-01 19:28:56 +0530

    Func1

---
+++ Func1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fun1(void)
+{
+	cout<<"Hello"<<endl;
+}
+
+void fun2(void)
+{
+	cout<<"People"<<endl;
+}
+
+int main()
+{
+	fun1();
+	fun2();
+	
+	return 0;
+}

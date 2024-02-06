0bd5459eb71b336dd6cac8268c958e8dd45bbbcd
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-06 18:58:24 +0530

    Help.java

---
+++ Help.java
@@ -0,0 +1,15 @@
+class Help
+{
+  public static void main(String[] args)
+ { 
+   System.out.println("Sun");
+   System.out.println("Mercury");
+   System.out.println("Venus " );
+   System.out.println("Earth");
+   System.out.println("Mars");
+   System.out.println("Jupiter");
+   System.out.println("Saturn");
+   System.out.println("Neptune");
+
+   }
+}

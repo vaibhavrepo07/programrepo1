fe06848c9fb0f9f6d04a8b3cf67a86a0bb5d1d53
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-06 18:59:42 +0530

    Mod.java

---
+++ Mod.java
@@ -0,0 +1,10 @@
+class Mod
+{
+   public static void main(String[]  args)
+{  
+   int a=10, b=20;
+   System.out.println("The Modulus Operator of the Two Numbers");
+   System.out.println(a%b);
+   System.out.println("The Modulus operator Run Successful");
+  }
+}

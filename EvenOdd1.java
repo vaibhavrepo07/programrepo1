598a426b4d66bb8dc4bc65630f45666f5fbbc1c1
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-06 18:49:22 +0530

    EvenOdd1.java

---
+++ EvenOdd1.java
@@ -0,0 +1,15 @@
+class EvenOdd1
+{
+  public static void main(String...args)
+{
+    int a=9;
+    if(a%2==0)
+ {
+   System.out.println("A is Even Number");
+  }
+ else
+{
+   System.out.println("A is Odd Number");
+ }
+}
+}

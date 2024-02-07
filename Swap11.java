9d07e73529c60332bcae52a8db5591286aa93440
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-07 19:39:24 +0530

    Swap11.java

---
+++ Swap11.java
@@ -0,0 +1,12 @@
+class Swap11
+{
+  public static void main(String...args)
+{
+  int a=30,b=50,c;
+  c=a;
+  a=b;
+  b=c;
+  System.out.println("The value of a " +a);
+  System.out.println("The value of b " +b); 
+  }
+}

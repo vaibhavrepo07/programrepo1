2b505e12580b9c9efb0b19c7ceab026abbf05ddd
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-07 19:45:32 +0530

    Nested.java

---
+++ Nested.java
@@ -0,0 +1,26 @@
+class Nested 
+{
+  public static void main(String[] args)
+ {
+   int a=300,b=500,c=600;
+   if(a>b)
+  {
+    if(a>c) 
+  {
+    System.out.printl("A is Greater than B"); 
+  }
+else
+{
+   System.out.println("C is Greater ");
+  }
+else if(b>c)
+{
+ System.out.println("B is Greater");
+  }
+else
+{
+  System.out.println("C is Greater");
+ }
+}
+}
+}

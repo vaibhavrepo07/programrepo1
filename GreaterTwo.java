f7dbd1b2444bf3ed45a6e3a1f5e56fa9edb0e9bd
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-07 19:43:46 +0530

    GreaterTwo.java

---
+++ GreaterTwo.java
@@ -0,0 +1,15 @@
+class Greatertwo 
+{
+   public static void main(String[] args)
+{ 
+   int a=10, b=29;
+   if(a>b)
+ { 
+   System.out.println("A is Greater than B");
+   } 
+
+ else
+{  
+  System.out.println("B is Greater than A"); 
+   }
+}

9ec7ca7086f17de47b7afd1026457cb6fb47a587
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-07 19:46:24 +0530

    Swapab.java

---
+++ Swapab.java
@@ -0,0 +1,12 @@
+class Swapab
+{
+  public static void main(String...args)
+{
+  int a=10,b=20;
+  a=a+b;
+  a=a-b;
+  b=a-b;
+  System.out.println("The value of A "+a);
+  System.out.println("The value of B "+b);
+  }
+}

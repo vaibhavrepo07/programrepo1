083f8e700082718377187b199aac37dd6e72edef
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-07 19:48:49 +0530

    EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+class EvenOdd
+{
+  public static void main(String...args)
+{
+   int no=90;
+   if(no%2==0)
+{
+   System.out.println("Number is Even");
+ }
+ else
+{
+  System.out.println("Number is Odd");
+ }
+}
+}

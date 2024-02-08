e042f4dc116996b4c9fcadf44fff889fa53075ec
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-08 19:32:36 +0530

    Area of Circle

---
+++ AreaofCircle.java
@@ -0,0 +1,13 @@
+class AreaofCircle
+{
+   public static void main(String...args)
+{
+   double rad;
+   final double PI=3.14;
+   
+   rad=10.0; 
+   
+   double area=PI * rad *rad;
+   System.out.println("Area of Circle is =" + area);
+   }
+}

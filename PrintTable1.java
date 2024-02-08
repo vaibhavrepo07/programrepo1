230ced1e55892d39d934053e044854fdaa0680ec
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-08 19:36:07 +0530

    PrintTable1.java

---
+++ PrintTable1.java
@@ -0,0 +1,12 @@
+class PrintTable1
+{
+  public static void main(String ...args)
+{
+   int no=5;
+   System.out.println("The Table of Five is ");
+   for(int i=1 ;i<=10; i++)
+{
+   System.out.println(+ i*no);
+    }
+   }
+ }

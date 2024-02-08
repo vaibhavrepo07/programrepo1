8c46aa673f51ecd98b4556440ebf636c41315ca7
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-08 19:35:27 +0530

    Factorial1.java

---
+++ Factorial1.java
@@ -0,0 +1,14 @@
+class Factorial1
+{
+  public static void main(String...args)
+{
+   int fact=1;
+   int no =19;
+   
+   for(int i=1; i<=no ; i++)
+{
+   fact = fact*i; 
+}
+  System.out.println("The factorial numbers is ="+ fact);
+   }
+}

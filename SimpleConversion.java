da32df011a0d582edfdb14ba04e2a599abc0fe95
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-08 19:36:56 +0530

    SimpleConversion.java

---
+++ SimpleConversion.java
@@ -0,0 +1,11 @@
+class SimpleConversion
+{
+  public static void main(String ...args)
+{
+   double n=54.53;
+   int a=(int)n; 
+   double p=(n-a)*100;
+   System.out.println("Rs: "+a);
+   System.out.println("Paise:"+(int)p); 
+   }
+}

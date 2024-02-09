610656a59d9c5c8156bdb9ea9754f56512b06d36
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-09 21:34:21 +0530

    BoolTest.java

---
+++ BoolTest.java
@@ -0,0 +1,17 @@
+class BoolTest
+{
+ public static void main(String...args)
+{ 
+   boolean b;
+   b=false; 
+   System.out.println("B is =" +b);
+   
+   b=true;
+   System.out.println("B is =" +b);
+   
+   if(b)
+{ 
+  system.out.println("This is Executed"); 
+   } 
+  }
+}

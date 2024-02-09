4ee18ebb37b52521b44eb7cc6ca8a806a6bb93c1
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-09 21:43:09 +0530

    Cmd.java

---
+++ Cmd.java
@@ -0,0 +1,12 @@
+class Cmd
+{
+public static void main(String[]x)
+{
+System.out.println(x[0]);
+System.out.println(x[1]);
+System.out.println(x[2]);
+System.out.println(x[3]);
+System.out.println(x[4]);
+System.out.println(x[5]);
+}
+}

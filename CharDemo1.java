185924fd7a0754fb0b3977d2b27e4a867cbf0d65
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-09 21:36:50 +0530

    CharDemo1.java

---
+++ CharDemo1.java
@@ -0,0 +1,12 @@
+class CharDemo1
+{
+   public static void main(String...args)
+{
+  char aChar, bChar;
+  aChar = 88;
+  bChar ='V';
+  
+  System.out.println("aChar and bChar"); 
+  System.out.println("The character "+bChar);
+  }
+}

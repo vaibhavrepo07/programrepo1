76310e4e9c5fa769140eecca6b9a7302840e6799
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-10 23:15:07 +0530

    InlineintSquareandCube

---
+++ InlineIntSquareandcube.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+inline int square(int a)
+{
+	return (a*a);
+}
+
+inline int cube(int b)
+{
+	return (b*b*b);
+}
+
+int main()
+{
+	cout<<"Square of the First Digit is ="<<square(5)<<endl;
+	cout<<"Cube of the Second Digit is ="<<cube(5)<<endl;
+	
+	return 0;
+}

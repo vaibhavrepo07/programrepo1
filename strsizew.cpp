60f2c4dfba52d60896451cb95474bbe4f1142bd7
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-10 23:18:54 +0530

    Strsize

---
+++ strsizew.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string s1 = "Vaibha";
+	
+	char ch   = 'v';
+	
+	char ch1[10] = "Hello";
+	cout<<strlen(ch1)<<endl;
+	cout<<"Add the last letter to complete the word"<<endl;
+	
+	str.push_back(ch);
+	cout<<"The String is = "<<str<<endl;
+	
+	return 0;
+}

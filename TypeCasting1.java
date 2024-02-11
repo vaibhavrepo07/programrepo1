afb7f622bcedf9c00718c8ce71ad0b21cc9ff77d
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-11 18:12:34 +0530

    TypeCasting1

---
+++ TypeCasting1.java
@@ -0,0 +1,13 @@
+class TypeCasting1
+{
+  public static void main(String...args)
+{
+  int a;
+  float b=134.222f;
+  System.out.println("Before Casting" +b);
+  
+  a = (int)b;
+   
+  System.out.println("After Casting" +a);
+  }
+}

f13ad67cc92f03449e7346ba60e5415a4f33bc2c
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-11 18:11:08 +0530

    Modulus.java

---
+++ Modulus.java
@@ -0,0 +1,10 @@
+class Modulus
+{
+   public static void main(String...args)
+{
+  int x=42;
+  double y =53.53;
+  System.out.println("x mod 10 =" +x%10);
+  System.out.println("y mod 10 =" +y%10);
+   }
+}

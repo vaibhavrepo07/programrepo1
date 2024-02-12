839bf3a48bdc3327c8b08bcc6927f28febbd471f
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-12 21:29:25 +0530

    OddEvenSum.java

---
+++ OddEvenSum.java
@@ -0,0 +1,22 @@
+class OddEvenSum
+{
+   public static void main(String ...args)
+{
+  int i; 
+ int sum=0;
+ int sum1= 0;
+ for(i=0 ;i<=10; i++)
+{
+   if(i%2==0)
+{
+   sum=sum+i;
+} 
+  else
+{
+   sum1 = sum1 + i;
+}
+  System.out.println("Sum of Even nUmbers "+ Sum);
+  System.out.println("Sum of Odd Numebrs is"+ Sum1);
+   }
+}
+}

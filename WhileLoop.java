1a3d6b65e992335859d7ee16623d5a08a2771d95
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-12 21:44:43 +0530

    WhileLoop

---
+++ WhileLoop.java
@@ -0,0 +1,21 @@
+class WhileLoop
+{
+  public static void main(String...args)
+{
+  int a=1;
+  while(a<=5)
+{
+  System.out.println(+a);
+  a++;
+}
+  
+ //exploring 
+
+  int sum=0, b=1;
+ while(b<=5)
+ sum+=b;
+ b++;
+ }
+ System.out.println("Sum is "+sum);
+   }
+}

1a8bb74dc0adc75ceba4bd9cb4747edead558bf2
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-13 21:29:43 +0530

    UTI

---
+++ uti.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sum(float a, int b)
+{
+	cout<<"The summation of two Numebrs is ="<<a+b<<endl;
+}
+
+ int main()
+ {
+ 	int a,b;
+ 	cout<<"Enter the First Digit"<<endl;
+ 	cin>>a;
+ 	cout<<"Enter the Second Digit"<<endl;
+ 	cin>>b;
+ 	
+ 	sum(a,b);
+ 	
+ 	return 0;
+ }

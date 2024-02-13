4c260d48668e66b3f411bc181c7e7e71d6049168
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-13 21:31:27 +0530

    SrtSize

---
+++ strSize.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string str1 = "Vaibhav";
+	
+	cout<<"The size of the String is ="<< sizeof(str1)<<endl;
+	cout<<"The size of the same string  is ="<< str1.size()<<endl;
+	
+	return 0;
+}

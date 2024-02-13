a073e26513eab94fc8135e6671b7101de23360de
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-13 21:33:28 +0530

    Strcat

---
+++ strcat.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	char str1[13]= "Sanskruti";
+	char str2[10]= "Vaibhav";
+	
+	cout<<"The bond between this love birds is Infinite"<<endl;
+	
+	cout<<strcat(str1, str2)<<endl;
+	
+	return 0;
+}

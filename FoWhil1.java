0b7189704d901f3e0248b9fcd70e3b2e28196bbe
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-14 23:53:36 +0530

    FoWhile1.java

---
+++ FoWhil1.java
@@ -0,0 +1,17 @@
+class FoWhil1
+{
+  public static void main(String...args)
+{
+  for(int i=1; i<=5; i++)
+{
+  int j = i ;
+ 
+  while(j<=5)
+ {
+   System.out.println(j);
+   j++;
+   }
+  System.out.println("");
+     }
+  }
+}

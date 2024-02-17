5aea923688613e2fa5af7914f64373d46f52c2b6
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-17 21:35:44 +0530

    CopyStringsStrcpy()

---
+++ CopysStringUsingstrcpy().cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int max=29;
+
+int main()
+{
+	int max;
+	char str1[]="www.QatarAriline";
+	char str2[max];
+	
+	cout<<"Coding with String Copy "<<endl;
+	strcpy(str2,str1);
+	
+	cout<<str2;
+	
+	return 0;
+	
+	
+	}

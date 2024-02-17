1b4c40d5eff2edc0681663d9389705d92b55d8b3
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-17 21:43:49 +0530

    TemplateinCharacter

---
+++ Template in Character.cpp
@@ -0,0 +1,17 @@
+//Progarm for Template 
+#include<bits/stdc++.h>
+using namespace std;
+
+template <class t1, class t2>
+void show(t1 a, t2 b)
+{
+	cout<<"a ="<< a<< "b ="<< b<<endl;
+}
+int  main()
+{
+	show(10 , 'B');
+
+	
+	
+	return 0 ;
+}

a05bb46e3d037872c77a51ed7ad75555f04d3866
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-17 21:42:54 +0530

    FunctionTemplate

---
+++ Function Template.cpp
@@ -0,0 +1,13 @@
+//Program for Function Templates
+#include<bits/stdc++.h>
+using namespace std;
+void sum(int a , int b)
+{
+	cout<<"Total is :"<< a+b<<endl;
+}
+int main()
+{
+	sum(2,3);
+	
+	return 0;
+}

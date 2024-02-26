9557096b7e7708bd232072774fd944e71e0c0214
vaibhavreop07v <vaiprogrammer.v762000@gmail.com>
2024-02-26 21:17:34 +0530

    ForEachLoop.java

---
+++ ForEachLoop.java
@@ -0,0 +1,18 @@
+class ForEachLoop
+{
+  public static void main(String...args)
+{
+  int arrayname[] = {1,3,5,7,9,11,13,15,17,19,21};
+
+ for(int val :arrayname)
+{
+  System.out.println("" +val);
+}
+ System.out.println();
+
+  for(float val : arrayname)
+{
+  System.out.println("" +val);
+     }
+   }
+}
